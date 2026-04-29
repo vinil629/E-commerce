7dfbf584856d793d0593f610c5047fb7b5bc87cd
vinil629 <reddyreddyvinilavr@gmail.com>
2026-04-29 11:40:57 +0530

    Server filr  is updated

--- Backend/Server.js
+++ Backend/Server.js
@@ -14,7 +14,7 @@ const mysql=require("mysql2");
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174","https://your-project.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
